ca56accc7ef01844ade8b2e869edc9d17bd17cc7
salin paul <salin.paul@geckolyst.com>
2024-03-12 16:04:12 +0530

    Fixing top nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,12 @@ import React, {useEffect, useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './SearchBox';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
   
   const getMovieRequest = async () => {
     const url = "http://www.omdbapi.com/?s=avengers&apikey=25cbda97"
@@ -22,6 +25,14 @@ const App = () => {
 
   return ( 
     <div className='container-fluid movie-app'>
+      <div className='row d-flex align-items-center mt-4 mb-4 fixed-top'>
+        <MovieListHeading heading="Movies"/>
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+      </div>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
       <div className='d-flex flex-nowrap'>
         <MovieList movies={movies} />
       </div>
